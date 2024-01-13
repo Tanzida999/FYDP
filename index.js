8e43fb2b2f5d8227d1db4fecf1896911a2ba070d
Tanzida Nowshin <nowshinsara555@gmail.com>
2024-01-13 09:51:21 +0600

    comment

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//Importing the necessary modules
+
 const express = require('express');
 const crypto = require('crypto');
 const ethers = require('ethers');
@@ -6,13 +8,17 @@ const path = require('path');
 const bodyParser = require('body-parser');
 app.use(express.static(__dirname));
 const jwt = require('jsonwebtoken');
+//Setting up middleware to handle  JSON  requests
 app.use(express.json());
 app.use(bodyParser.json());
-
+//Handling get requests to the root URL ("/")
 app.get('/',(req,res) => {
+    //Sending the 'index.html' in response
     res.sendFile(path.join(__dirname + '/index.html'));
 })
 
+app.get()
+//Starting the server on port no. 3000
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 })
